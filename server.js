3e7d120b547eabdfda53f91bf237c0ebcb7b9e80
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-01 00:14:25 +0530

    using PM2 to create clusters

--- server.js
+++ server.js
@@ -24,17 +24,5 @@ app.get('/timer', (req, res) => {
     res.send(`Ding dong! - ${process.pid}`);
 });
 
-if (cluster.isMaster) {
-    console.log('Master has been started...');
-    // total number of logical CPU cores
-    const NUM_WORKERS = os.cpus().length;
-    console.log('Total logical CPU cores:', NUM_WORKERS);
-
-    // total number of worker process = total number of logical CPU cores
-    for (let i = 0; i < NUM_WORKERS; i++) {
-        cluster.fork();
-    }
-} else {
-    console.log('Worker process started.');
-    app.listen(8000);
-}
+console.log('Worker process started.');
+app.listen(8000);
